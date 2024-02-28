85d90163ad7b4c2d0967a18bad8fe6ef6aa643db
Marvin Sojo <119646998+marvinsojo@users.noreply.github.com>
2024-02-28 03:27:55 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState("");
+  const [tasksList, setTasksList] = useState([]);
+
+  const handleTask = (e) => {
+    if (e.key == "Enter") {
+      setTasksList([
+        ...tasksList,
+        {
+          label: task,
+          done: false,
+        },
+      ]);
+    }
+
+    setTask(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="bg-dark min-vh-100 d-flex justify-content-center ">
+        <div className="bg-light w-50 mh-75 my-3">
+          <div className="text-center mt-4">
+            <h1 className="task-title">todos</h1>
+          </div>
+          <div className="task-body m-5 p-2">
+            <div className="text-center">
+              <input
+                className="task-in border-0 form-control text-body-secondary"
+                type="text"
+                placeholder="Task"
+                onKeyUp={(evt) => handleTask(evt)}
+              />
+            </div>
+
+            {tasksList.map((toDo, index) => {
+              return (
+                <div className="task-out form-control border-0 text-body-secondary my-1" key={index}>
+                  {toDo.label}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
