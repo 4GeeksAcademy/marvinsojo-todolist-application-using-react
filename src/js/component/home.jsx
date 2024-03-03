c4ff7fe3165cde2056ff20952d16020285804610
Marvin Sojo <119646998+marvinsojo@users.noreply.github.com>
2024-03-03 04:33:23 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
   const [task, setTask] = useState("");
+
   const [tasksList, setTasksList] = useState([]);
 
-  const handleTask = (e) => {
-    if (e.key == "Enter") {
+  const handleTask = (event) => {
+    if (event.key == "Enter") {
       setTasksList([
         ...tasksList,
         {
@@ -16,7 +17,12 @@ const Home = () => {
       ]);
     }
 
-    setTask(e.target.value);
+    setTask(event.target.value);
+  };
+
+  const deleteTask = (index) => {
+    const newListTask = tasksList.filter((task, i) => i !== index);
+    setTasksList(newListTask);
   };
 
   return (
@@ -24,26 +30,42 @@ const Home = () => {
       <div className="bg-dark min-vh-100 d-flex justify-content-center ">
         <div className="bg-light w-50 mh-75 my-3">
           <div className="text-center mt-4">
-            <h1 className="task-title">todos</h1>
+            <h1 className="task-title display-1">todos</h1>
           </div>
-          <div className="task-body m-5 p-2">
+
+          <div className="task-body mx-5 mb-0 mt-5 p-2">
             <div className="text-center">
               <input
-                className="task-in border-0 form-control text-body-secondary"
+                className="task-in border-0 form-control px-3"
                 type="text"
-                placeholder="Task"
+                placeholder=""
                 onKeyUp={(evt) => handleTask(evt)}
               />
             </div>
 
             {tasksList.map((toDo, index) => {
               return (
-                <div className="task-out form-control border-0 text-body-secondary my-1" key={index}>
-                  {toDo.label}
+                <div
+                  className="task-body-out row mx-1 my-2 py-2"
+                  key={index}
+                >
+                  <div className="task-out border-0 text-body-light col-md-10">
+                    {toDo.label}
+                  </div>
+                  <div className="col-md-2 d-flex justify-content-center align-items-center">
+                    <button
+                      className="delete btn rounded-circle text-light border-light"
+                      onClick={() => deleteTask(index)}
+                    >
+                      X
+                    </button>
+                  </div>
                 </div>
               );
             })}
           </div>
+          <div className="efecto-pagina-2 mx-5"></div>
+          <div className="efecto-pagina-3 mx-5"></div>
         </div>
       </div>
     </>
